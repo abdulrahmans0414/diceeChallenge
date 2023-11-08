efd39b63528f7d84941fb95c8ca2cf7caa767f36
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-09 01:54:26 +0530

    add script for dice roled and also second dice roled if refresh the page with random 1 to 6

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Working !");
+var randomNumber1 =Math.floor(Math.random()*6) +1; // random number between 1 and 6
+
+var randomDiceImage = "dice"+randomNumber1 +".png"; //dice1.png to dice6.png
+
+var randomImageSource = "images/"+randomDiceImage; // images/dice1.png to images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+// for second image
+var randomNumber2 =Math.floor(Math.random()*6) +1; 
+
+var randomImageSource2= "images/dice"+randomNumber2 + ".png"; 
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);

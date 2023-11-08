3cccc146cffdcffbf10afa22b0b80f658de590a0
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-09 02:30:42 +0530

    add show result as wins player

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ var randomNumber2 =Math.floor(Math.random()*6) +1;
 var randomImageSource2= "images/dice"+randomNumber2 + ".png"; 
 
 var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+// create a message to show greater numbers of dice
+// player 1 wins
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "player 1 wins!";
+}
+// player 2 wins
+else if (randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML = "player 2 wins!";
+
+}
+// display draw
+else{
+    document.querySelector("h1").innerHTML = "Draw !";
+}
